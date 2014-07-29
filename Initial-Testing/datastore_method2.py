1521fd29c50741f6158e9528c49eaec722481c49
Jacob Carter <jcarter@bb-jcarter-mbp.edline.com>
2014-07-29 15:10:00 -0500

    Vsphere to Graphite Performance Metrics Proxy
    Written in python using the pysphere api, this script will mine
    performance metrics from VSphere and push them to Graphite.

---
+++ Initial-Testing/datastore_method2.py
@@ -0,0 +1,20 @@
+
+from pysphere import VIServer, VIProperty 
+
+server = VIServer() 
+HOST = "HostNameOrIPAddress"
+USER = "username"
+PASSWORD =  "password"
+
+server.connect(HOST, USER, PASSWORD) 
+
+for ds_mor, name in server.get_datastores().items(): 
+    props = VIProperty(server, ds_mor) 
+    print "DATASTORE:", name 
+    print "  Type:", props.summary.type 
+    print "  Capacity:", props.summary.capacity 
+    print "  Free space:", props.summary.freeSpace 
+    if hasattr(props.summary, "uncommitted"): 
+        print "  Uncommited:", props.summary.uncommitted 
+
+server.disconnect() 
